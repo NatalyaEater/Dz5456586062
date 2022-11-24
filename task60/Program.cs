8ea957d3c469401d8811b175e788a8aa4d3faec8
Natasha Eater <natalya.iter@yandex.ru>
2022-11-24 19:01:45 +0300

    4

--- task60/Program.cs
+++ task60/Program.cs
@@ -1,2 +1,51 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 60.Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+// Массив размером 2 x 2 x 2
+// 66(0,0,0) 25(0,1,0)
+// 34(1,0,0) 41(1,1,0)
+// 27(0,0,1) 90(0,1,1)
+// 26(1,0,1) 55(1,1,1)
+
+Console.WriteLine("Введите три аргумента размерности трехмерного массива");
+Console.WriteLine("Введите первый аргумент:");
+int oneArgyment = int.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine("Введите второй аргумент:");
+int twoArgyment = int.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine("Введите третий аргумент:");
+int threeArgyment = int.Parse(Console.ReadLine() ?? "0");
+
+int[,,] matrix = new int[oneArgyment, twoArgyment, threeArgyment];
+GetArray(matrix);
+PrintArray(matrix);
+
+void GetArray(int[,,] inArray)
+{
+    int step = 10;
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            for (int f = 0; f < inArray.GetLength(2); f++)
+            {
+                inArray[i, j, f] = step;
+                step++;
+            }
+        }
+    }
+}
+
+
+
+void PrintArray(int[,,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int f = 0; f < array.GetLength(2); f++)
+            {
+                Console.Write("{0,5:F0}", array[i, j, f] + "" + "(" + i + "," + j + "," + f + ")\t");
+            }
+            Console.WriteLine();
+        }
+    }
+}
--- task62/Program.cs
+++ task62/Program.cs
@@ -1,2 +1,61 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 62. Напишите программу, которая заполнит спирально массив 4 на 4.
+// Например, на выходе получается вот такой массив:
+// 01 02 03 04
+// 12 13 14 05
+// 11 16 15 06
+// 10 09 08 07
+
+
+int[,] matrix = new int[4, 4];
+PrintArray(matrix);
+FillArray(0,0);
+PrintArray(matrix);
+
+
+void FillArray(int row, int col)
+{
+    if (matrix[row, col] == 0)
+    {
+        matrix[row, col] = 1;
+        FillArray(row - 1, col);
+        FillArray(row, col - 1);
+        FillArray(row + 1, col);
+        FillArray(row, col + 1);
+
+    }
+}
+void  FillArray(int[,] array)
+{
+    int step = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = step;
+            step++;
+            Console.Write("{0,5:F0}", array[i, j] + " ");
+        }
+        if(i==array.GetLength(0)) 
+        {
+            j++
+        }
+        Console.WriteLine();
+    }
+}
+
+
+
+void PrintArray(int[,] array)
+{
+    int step = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = step;
+            step++;
+            Console.Write("{0,5:F0}", array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
